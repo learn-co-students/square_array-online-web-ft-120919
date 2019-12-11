60a298b9ccf68090a3d565d30d9654eafecb6664
Ho Yin Keung <hykeung92@gmail.com>
2019-12-11 19:25:50 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  ar = []
+  array.each do |i|
+  new_numbers = i**2
+  ar << new_numbers
+  end
+  ar
+end
+
+def new_array_collect(array)
+  new_numbers.collect{|i| i**2}
 end
